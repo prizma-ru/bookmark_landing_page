7e6ee611c3d250233d6a0f44bc60685e95d4d433
prizma-ru <dmitry.glotov@gmail.com>
2020-11-07 20:03:48 +0300

    добавил аккардион и js функцию, как работает на разбирался но вроде это jquery <script src=https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js></script>

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(function() {
+
+
+
+
+    /* Collapse */
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+    });
+
+
+
+
+});
